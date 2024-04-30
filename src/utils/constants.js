8b4c5ba780a5ece487994771c2be8901cf5a91bd
tripathianurag63 <anuragtripathi209306@gmail.com>
2024-04-30 17:53:48 +0530

    TMDB API data Fetch & built MainContainer

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+  export const API_OPTIONS = {
+    method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWZkNjQ2OTc4NzQ0NWMzNmYzNjhhNjY0NTI4NWViNiIsInN1YiI6IjY2MTdhY2VkN2Q0MWFhMDE3ZDAwN2Y0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OHDX0nRAyuC2CHG4FLAhH_lncWrFk_cGHYdgq799jBA'
+  },
+  };
